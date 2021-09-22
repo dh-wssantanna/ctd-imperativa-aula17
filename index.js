90d2dbbbfbb641d1132f06792bd346fccedec00b
Willian Sant Anna <w.s.santanna@gmail.com>
2021-09-22 20:21:48 -0300

    Implementação do script do código de apresentação e mesa de trabalho.

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+    Aula 17 - Express: Introdução
+    - Instação e configuração npm;
+    - Instalação do Nodemon(https://www.npmjs.com/package/nodemon);
+    - Instalação do Express(https://expressjs.com/pt-br/);
+    - Configurando as excessões do git com .gitignore;
+    - Rotas Express.
+*/
+
+// 1. Importar o módulo Express.
+
+// 2. Inicializar o contrutor do Express.
+
+// 3. Criando a Rota e retornando alguma informação.
+
+// 4. Inicializar o servidor.
