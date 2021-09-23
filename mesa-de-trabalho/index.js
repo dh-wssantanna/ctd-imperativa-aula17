861acf904f839f02da5cf6eb3dd4db73714af9ef
Raimundo123321 <raimundononatobatistaneto@gmail.com>
2021-09-22 22:32:29 -0300

    Execução da mesa de trabalho 17

--- mesa-de-trabalho/index.js
+++ mesa-de-trabalho/index.js
@@ -1,20 +1,34 @@
 /*
     Atividade I: Inicialize um projeto npm;
-    (  ) Preencha nome do projeto;
-    (  ) Preencha o nome de todos os integrantes do grupo;
-    (  ) Preencha a licença como UNLICENSED;
-    (  ) Preencha a versão como 1.0.
+    ( x ) Preencha nome do projeto;
+    ( x ) Preencha o nome de todos os integrantes do grupo;
+    ( x ) Preencha a licença como UNLICENSED;
+    ( x ) Preencha a versão como 1.0.
 
     Atividade II: Instale o Nodemon globalmente em sua máquina. Link com a instrução de instalação: https://www.npmjs.com/package/nodemon.
     
     Atividade III: Ignore a pasta node_modules
-    (  ) Configure o git, utilizando o arquivo .gitignore, para que ele ignore as alterações realizas na pasta node_modules.
+    ( x ) Configure o git, utilizando o arquivo .gitignore, para que ele ignore as alterações realizas na pasta node_modules.
 
     Atividade IV: Crie o projeto 
-    (  ) Instale o Express no projeto (https://expressjs.com/pt-br/),
-    (  ) Implemente a aplicação como as seguintes rotas:
-        (  ) Rota http://localhost:1234/
-            (  ) Insira o texto: Seja bem-vindo ao [nome_do_projeto].
-        (  ) Rota http://localhost:1234/sobre/
-            (  ) Insire o texto: Sobre o projeto
+    ( x ) Instale o Express no projeto (https://expressjs.com/pt-br/),
+    ( x ) Implemente a aplicação como as seguintes rotas:
+        ( x ) Rota http://localhost:1234/
+            ( x ) Insira o texto: Seja bem-vindo ao [nome_do_projeto].
+        ( x ) Rota http://localhost:1234/sobre/
+            ( x ) Insire o texto: Sobre o projeto
 */
+
+const express = require('express');
+
+const app = new express();
+
+app.get('/', function(req, res) {
+    res.send('Seja bem-vindo a mesa-de-trabalho-17')
+});
+
+app.get('/sobre', function(req, res) {
+    res.send('A mesa de trabalho 17 nós apreseta a novas ferramentas que vão nos permitir criar um servidor de forma local');
+});
+
+app.listen(1234);
